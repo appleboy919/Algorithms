a1658cd776f76cc6327a379729593ef41ed9e8a1
appleboy919 <doh24@wisc.edu>
2021-02-23 02:02:05 +0900

    start: pefromance scheduling on N cities

---
+++ Graph/performance_schedule.py
@@ -0,0 +1,8 @@
+# N cities, M ways
+# Mon - Sat: all different
+# new Mon = prv Sat
+# city 1 on 1:Mon ==> N: last Sat (Few as possible)
+# Output: shortest time or 1(impossible)
+# Input:
+#   1: N(cities) M(roads)
+#   M lines: u v (u<->v && u!=v)
